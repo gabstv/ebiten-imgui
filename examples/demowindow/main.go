f215bf6cfcc08d2a4a3d2318dbab51b980de61f4
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-07-10 16:59:46 -0300

    demo window example

---
+++ examples/demowindow/main.go
@@ -0,0 +1,73 @@
+// +build example
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/gabstv/ebiten-imgui/renderer"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/inkyblackness/imgui-go/v2"
+)
+
+func main() {
+	mgr := renderer.New(nil)
+
+	ebiten.SetWindowSize(1024, 768)
+	ebiten.SetWindowResizable(true)
+
+	gg := &G{
+		mgr:    mgr,
+		dscale: ebiten.DeviceScaleFactor(),
+	}
+
+	ebiten.RunGame(gg)
+}
+
+type G struct {
+	mgr *renderer.Manager
+	// demo members:
+	showDemoWindow bool
+	dscale         float64
+	retina         bool
+	w, h           int
+}
+
+func (g *G) Draw(screen *ebiten.Image) {
+	g.mgr.BeginFrame()
+
+	{
+		imgui.Checkbox("Retina", &g.retina) // Edit bools storing our window open/close state
+
+		imgui.Checkbox("Demo Window", &g.showDemoWindow) // Edit bools storing our window open/close state
+
+		if g.showDemoWindow {
+			imgui.ShowDemoWindow(&g.showDemoWindow)
+		}
+	}
+
+	g.mgr.EndFrame(screen)
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %.2f", ebiten.CurrentTPS()))
+}
+
+func (g *G) Update(screen *ebiten.Image) error {
+	g.mgr.Update(1.0/60.0, float32(g.w), float32(g.h))
+	return nil
+}
+
+func lerp(a, b, t float64) float64 {
+	return a*(1-t) + b*t
+}
+
+func (g *G) Layout(outsideWidth, outsideHeight int) (int, int) {
+	if g.retina {
+		m := ebiten.DeviceScaleFactor()
+		g.w = int(float64(outsideWidth) * m)
+		g.h = int(float64(outsideHeight) * m)
+	} else {
+		g.w = outsideWidth
+		g.h = outsideHeight
+	}
+	return g.w, g.h
+}
